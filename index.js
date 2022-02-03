aa84e06a9f6cc2f7a77d850fc79f99ac0ab201a2
hari-19 <hari19@outlook.in>
2022-02-03 21:48:11 +0530

    a

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+ * create an orbitDB key-value database of pet animals
+ * July 2021 - (c) Vu Tien Khang - Machu Picchu
+ */
+// import the packages
+const IPFS = require('ipfs');
+const OrbitDB = require('orbit-db');
+
+async function main () {
+    const ipfsOptions = {   };    // placeholder
+    const ipfs = await IPFS.create(ipfsOptions);
+    console.log('-> IPFS node connected');
+    const orbitdb = await OrbitDB.createInstance(ipfs);
+    console.log('-> OrbitDB instance created');
+    
+    // First stage, create and populate database with 2 key-value pairs
+    // in OrbitDB, 1 key-value database = 1 pet
+    const db1 = await orbitdb.keyvalue('pet-animal');
+    await db1.put('species', 'German Shepherd', { pin: true });
+    await db1.put('owner', 'myself', { pin: true });
+    console.log('-> One pet animal created');
+    await db1.close();
+
+    // 2nd stage, reopen and retrieve from the database
+    const db2 = await orbitdb.keyvalue('pet-animal');
+    await db2.load();
+    let species = db2.get('species');
+    console.log(' -> database value of "species" = ', species);
+    let owner = db2.get('owner');
+    console.log(' -> database value of "owner" = ', owner);
+    await db2.close();
+}
+main();
