24888a121c95d40a5d12769343ba30a7122932b3
hari-19 <hari19@outlook.in>
2022-02-05 08:32:10 +0530

    gun 1

--- gun.js
+++ gun.js
@@ -2,10 +2,6 @@ const GUN = require("gun");
 
 gun = GUN(["172.26.240.1", "20.204.139.208"]);
 
-alice.on(function(node){
-    console.log('Subscribed to Alice!', node);
-});
-
 gun.get('bob').once(function(node){
     console.log('Bob!', node);
 });
